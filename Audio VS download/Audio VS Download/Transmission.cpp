1539948f4b21d98ff5287ca12001f85cf3626738
JesseRussell2240 <jrussell2240@conestogac.on.ca>
2023-11-11 14:32:57 -0500

    Modified recive message to take in pointers. Added encrypt into message

--- Audio VS download/Audio VS Download/Transmission.cpp
+++ Audio VS download/Audio VS Download/Transmission.cpp
@@ -11,13 +11,13 @@ Details: Contains various audio file functions such as , play, save, load, recor
 #include "RS232Comm.h"
 #include "Transmission.h"
 #include "AudioRecorder.h"
-#include "encrypt.h"
+
 
 //dosables warning for userinput
 #pragma warning	(disable:4996)
 
-extern wchar_t COMPORT_Rx[];
-extern wchar_t COMPORT_Tx[];
+//extern wchar_t COMPORT_Rx[];
+extern wchar_t COMPORT[];
 
  // Declare constants, variables and communication parameters
 const int BUFSIZE = 280;							// Buffer size
@@ -31,36 +31,26 @@ extern short iBigBuf[];								// Declare the external variable
 extern long lBigBufSize;							// Declare the external variable
 
 
-
-
-
-
-
+//setter function for comBits
 void setComBits(int bits) {
 	nComBits = bits;
 }
 
+//setter function for comRate
 void setComRate(int rate) {
 	nComRate = rate;
 }
 
-void initializeRxPort(const wchar_t* port) {
-	initPort(&hComRx, COMPORT_Rx, nComRate, nComBits, timeout);
-	Sleep(500);
+//port initilization helper
+void initializePort(const wchar_t* port) {
+	initPort(&hComRx, COMPORT, nComRate, nComBits, timeout);
+	Sleep(50);
 }
-
-void initializeTxPort(const wchar_t* port) {
-	initPort(&hComTx, COMPORT_Tx, nComRate, nComBits, timeout);
-	Sleep(500);
-}
-
 
 //removed out code to keep the recive port open and appending a special ending to the message
 void transmitMessage(const char* msg) {
 	char msgWithSenderID[BUFSIZE];
 	//snprintf(msgWithSenderID, BUFSIZE, "%s %s", msg, senderID); // Append the sender ID to the message
-
-	//XOR encode funtion
 
 	outputToPort(&hComTx, msg, strlen(msg) + 1);
 	Sleep(500);
@@ -68,55 +58,42 @@ void transmitMessage(const char* msg) {
 	CloseHandle(hComTx);
 }
 
-void receiveMessages() {
-	char msgIn[BUFSIZE];
-
-
+void receiveMessages(char* msgBuffer, int* msgLength) {
 	Sleep(1500);
 	DWORD bytesRead;
-	bytesRead = inputFromPort(&hComRx, msgIn, BUFSIZE);
-	msgIn[bytesRead] = '\0';
-	printf("Received Message: %s\n", msgIn);
-//	printf("Message: %s\n", msgIn);
+	bytesRead = inputFromPort(&hComRx, msgBuffer, BUFSIZE);
+	msgBuffer[bytesRead] = '\0';
+	*msgLength = bytesRead;
+	printf("Received Message: %s\n", msgBuffer);
 
 	purgePort(&hComRx);
 	CloseHandle(hComRx);
-
 }
 
 void transmitAudio(short* audioData, int dataSize) {
-	//const char* charData = reinterpret_cast<const char*>(audioData);
-	
-	//printf("size of transmitted audio is %d\n", dataSize * sizeof(short) * 2);
-	//char* pntr = (char*)audioData;
-	//PlayAudio(audioData, dataSize);
-
-	//outputToPort(&hComTx, (char*)&audioData, dataSize * sizeof(short) * 2);
-	outputToPort(&hComTx, (char*) audioData, 40000 * 2);
-
+	outputToPort(&hComTx, (char*) audioData, dataSize * sizeof(short));
 	purgePort(&hComTx);
 	CloseHandle(hComTx);
 }
 
 void receiveAudio(short* audioData, int dataSize) {
 
-
 	DWORD bytesRead;
 	printf("recive audio called\n");
-	//bytesRead = inputFromPort(&hComRx, (char*)audioData, dataSize * sizeof(short));
 	bytesRead = inputFromPort(&hComRx, (char*)audioData, dataSize * sizeof(short));
 	printf("bytes expected %d\n", dataSize);
 	
 	if (bytesRead == dataSize * sizeof(short)) {
 		// The received data size matches the expected size
+		PlayAudio(audioData, dataSize);
 		
 	}
 	else {
-		printf("error in recive audio helper function");
+		printf("error in recive audio. Expected size =! recived size...");
 		// Handle an error, as the received data size is not as expected
 	}
 
-	PlayAudio(audioData, dataSize);
+	
 
 	purgePort(&hComRx);
 	CloseHandle(hComRx);
--- Audio VS download/Audio VS Download/main.cpp
+++ Audio VS download/Audio VS Download/main.cpp
@@ -16,6 +16,7 @@ Details: Tersting mainline for sub programs Transmission.cpp and AudioRecorder.c
 #include <time.h>
 #include "message.h"
 #include <stdlib.h>
+#include "encrypt.h"
 					
 //these are buffers for storing the audio message
 //extern short iBigBuf[];  // Declare the external variable// buffer
@@ -27,8 +28,8 @@ Details: Tersting mainline for sub programs Transmission.cpp and AudioRecorder.c
 // A commtimeout struct variable
 //wchar_t COMPORT_Rx[] = L"COM8";
 //wchar_t COMPORT_Tx[] = L"COM9";
-wchar_t COMPORT_Rx[] = L"COM6";
-wchar_t COMPORT_Tx[] = L"COM7";
+wchar_t COMPORT[] = L"COM6";
+
 
 
 extern short iBigBuf[];								// Declare the external variable
@@ -65,16 +66,18 @@ int	main(int argc, char* argv[])
         printf("4. Save audio in buffer\n");
         printf("5. Transmit audio in buffer\n");
         printf("6. Transmit text message in buffer\n");
-		printf("7. Random\n");
+		//printf("7. Random\n");
         printf("Enter your choice: ");
         scanf_s("%d", &option);
 
         //used to flush the input buffer when user incorectly enters input
         while ((c = getchar()) != '\n' && c != EOF) {}
 
+
         //switch case for various user inputs
-        switch (option) { // Recording audio case
-        case 1:
+        switch (option) { 
+
+        case 1:	// Recording audio case
       
             //time duration userinput code
             printf("Enter the  duration \n");
@@ -99,15 +102,14 @@ int	main(int argc, char* argv[])
             break;
 
         case 3:
+			//user option for loading into buff from audio file
 
             //file loading code
             printf("Enter the path to the audio file to load: ");
             fgets(filePath, sizeof(filePath), stdin);
-
             //remove line end char
             n = strlen(filePath);  // Assign to 'n' (declared before the switch)
             filePath[n - 1] = '\0';
-
             //move audio to buffer
             LoadAudio(iBigBuf, lBigBufSize, filePath);
 
@@ -128,186 +130,174 @@ int	main(int argc, char* argv[])
 
             //move audio to buffer
             SaveAudio(iBigBuf, lBigBufSize, filePath);
-
-            //calls helper function to play audio recording
             printf("\Saved audio to file ...\n");
-           
             break;
 
         case 5:
 
-				
-                printf("What COMM port would you like to use (1-9): ");
-                int comPortNumber;
-                scanf_s("%d", &comPortNumber);
+			//gets user input for comport setup	
+            printf("What COMM port would you like to use (1-9): ");
+			int comPortNumber;
+            scanf_s("%d", &comPortNumber);
+			wchar_t TempPort[20];
+			swprintf(TempPort, L"COM%d", comPortNumber);
+			wcscpy(COMPORT, TempPort);             
 
-                // Check if the COM port number is within the valid range
-              if (comPortNumber >= 1 && comPortNumber <= 9) {
-                wchar_t Port[20];
-                swprintf(Port, L"COM%d", comPortNumber);
+			//request transmission or reciving
+			printf("Options:\n");
+			printf("1. Transmit\n");
+			printf("2. Receive\n");
 
-               
-               }
+			printf("Enter your choice (1, or 2): ");
+			scanf_s(" %c", &option, 1);
 
-				//once user selects Physical com ports it stays in this section of code
-				//while (1) {
+			if (option == '1') {
+
+				printf("Enter the bit rate: ");
+				int rate;
+				scanf("%d", &rate);
+				setComRate(rate);
+				initializePort(COMPORT);
+
+				transmitAudio(iBigBuf, lBigBufSize);
+			}
+
+			else if (option == '2') {
+
+				printf("Enter the number of bits: ");
+				int bits;
+				scanf("%d", &bits);
+				setComBits(bits);
+
+				printf("Enter the bit rate: ");
+				int rate;
+				scanf("%d", &rate);
+				setComRate(rate);
+
+				initializePort(COMPORT);
+				receiveAudio(iBigBuf, lBigBufSize); // Pass the buffer to store the received audio
+
+			}
+			else {
+
+				printf("Invalid input. Please enter 1 or 2.\n");
+
+			}
+            break;
+
+           case 6:
+
+			   //updated comm port number based on user provided input
+			   //var initilization is in case 5 - thiscould be a sub function to reduce redunduncy, but so could the transmit/recive request
+			   printf("What COMM port would you like to use (1-9): ");
+			   scanf_s("%d", &comPortNumber);
+			   swprintf(TempPort, L"COM%d", comPortNumber);
+			   wcscpy(COMPORT, TempPort);
+
+			   char userResultTwo;
+
 				printf("Options:\n");
 				printf("1. Transmit\n");
 				printf("2. Receive\n");
+				printf("Enter your choice (1 or 2): ");
+				scanf_s(" %c", &userResultTwo, 1);
 
-				printf("Enter your choice (1, or 2): ");
-				scanf_s(" %c", &option, 1);
+				if (userResultTwo == '1') {
+					// Transmit text message
+					printf("Enter the bit rate: ");
+					int rate;
+					scanf("%d", &rate);
+					setComRate(rate);
 
-				if (option == '1') {
+					initializePort(COMPORT);
 
-					// This code does not correctly update the comm port based on user input, i commentd it out but this needs to be figure out
-					printf("Enter the COM port: ");
-					wchar_t txPort[20];
-					scanf_s("%s", txPort, 20);
-					
-			
-	
+					// User input for text message\
+
+					char userResultThree;
+
+					// Physical COM Port selected
+					printf("Options:\n");
+					printf("1. Custom Message\n");
+					printf("2. Genorate Message\n");
+					printf("Enter your choice (1 or 2): ");
+					scanf_s(" %c", &userResultThree, 1);
+
+					char msgOut[250];
+
+					if (userResultThree == '1') {
+
+						printf("Enter the text message to transmit: ");
+						scanf(" %[^\n]s", msgOut);
+					}
+					else if (userResultThree == '2') {
+						numberOfQuotes = fnumQuotes();
+						quoteIndices = fquoteIndices(numberOfQuotes);
+						quoteLengths = fquoteLength(numberOfQuotes, quoteIndices);
+						randomNum = frandNum(0, numberOfQuotes - 1);
+						char messageBuffer[MAX_QUOTE_LENGTH];
+						bytesRead = GetMessageFromFile(messageBuffer, MAX_QUOTE_LENGTH, randomNum, numberOfQuotes, quoteIndices, quoteLengths);
+
+						// Print the random message
+						printf("\nRandom Message:\n%s\n", messageBuffer);
+						strcpy(msgOut, messageBuffer);
+						free(quoteIndices);
+						free(quoteLengths);
+					}
+
+					//encrypt transmitted message
+					//XOR encode funtion
+					//args in the following order to encrypt (message, messageLen, secretKey, secretKeyLen, encBuf
+					char secretKey[10] = "314159265";
+					int keyLength = 10;
+					char tempBuf[250];
+					xorCipher(msgOut, strlen(msgOut), secretKey, keyLength, tempBuf);
+
+
+					// Transmit text message
+					transmitMessage(msgOut);
+
+				}	else if (userResultTwo == '2') {
+					// Receive text message
+
 
 					printf("Enter the bit rate: ");
 					int rate;
 					scanf("%d", &rate);
 					setComRate(rate);
 
-					initializeTxPort(COMPORT_Tx);
-
-					//const short msgConverted = msgOut[];
-					transmitAudio(iBigBuf, lBigBufSize);
-
-				}
+					initializePort(COMPORT);
 
 
-				else if (option == '2') {
+					int messageLength;
+					char messageBuffer[250];
+					// Receive text message
+					receiveMessages(messageBuffer, &messageLength);
 
-					wchar_t rxPort[20];
-					
-					printf("Enter the COM port: ");
-
-
-					scanf_s("%s", rxPort, 20);
-
-					printf("Enter the number of bits: ");
-					int bits;
-					scanf("%d", &bits);
-					setComBits(bits);
-					
-
-					printf("Enter the bit rate: ");
-					int rate;
-					scanf("%d", &rate);
-					setComRate(rate);
-
-					initializeRxPort(COMPORT_Rx);
-					//short receivedAudio[AUDIO_BUFFER_SIZE]; // Define a buffer to store received audio
-					receiveAudio(iBigBuf, lBigBufSize); // Pass the buffer to store the received audio
 
 				}
 				else {
 					printf("Invalid input. Please enter 1 or 2.\n");
 				}
-				//}
-
-				//system("pause");
-
-			//return 0;
-
-            break;
-
-           case 6:
-			   char userResultTwo;
-
-				   // Physical COM Port selected
-				   printf("Options:\n");
-				   printf("1. Transmit\n");
-				   printf("2. Receive\n");
-				   printf("Enter your choice (1 or 2): ");
-				   scanf_s(" %c", &userResultTwo, 1);
-
-				   if (userResultTwo == '1') {
-					   // Transmit text message
-					   printf("Enter the bit rate: ");
-					   int rate;
-					   scanf("%d", &rate);
-					   setComRate(rate);
-
-					   initializeTxPort(COMPORT_Tx);
-
-					   // User input for text message
-					   printf("Enter the text message to transmit: ");
-					   char msgOut[250];
-					   scanf(" %[^\n]s", msgOut);
-
-					   // Transmit text message
-					   transmitMessage(msgOut);
-
-				   }	else if (userResultTwo == '2') {
-					   // Receive text message
-					   printf("Enter the bit rate: ");
-					   int rate;
-					   scanf("%d", &rate);
-					   setComRate(rate);
-
-					   initializeRxPort(COMPORT_Rx);
-
-					   // Receive text message
-					   receiveMessages();
-				   }
-				   else {
-					   printf("Invalid input. Please enter 1 or 2.\n");
-				   }
 			
-			   break;
+			break;
+
+
 		   case 7:
 
-			   numberOfQuotes = fnumQuotes();
-			   quoteIndices = fquoteIndices(numberOfQuotes);
-			   quoteLengths = fquoteLength(numberOfQuotes, quoteIndices);
-			   randomNum = frandNum(0, numberOfQuotes - 1);
-			   char messageBuffer[MAX_QUOTE_LENGTH];
-			   bytesRead = GetMessageFromFile(messageBuffer, MAX_QUOTE_LENGTH, randomNum, numberOfQuotes, quoteIndices, quoteLengths);
+			   printf("Option 7 coming soon.\n");
 
-			   // Print the random message
-			   printf("\nRandom Message:\n%s\n", messageBuffer);
-			   free(quoteIndices);
-			   free(quoteLengths);
-
-			   break;
+			break;
 
         default:
-         //   system("cls");
-          //  printf("Invalid option. Please choose a valid option.\n");
+            //system("cls");
+            printf("Invalid option. Please choose a valid option.\n");
         }
 
     } while (option != -1);
 
+
+
+
+
     return 0;
 }
-/*
-  srand(time(NULL));
-
-			   // Get the number of quotes in the file
-			   int numQuotes = fnumQuotes();
-
-			   // Generate a random number between 0 and numQuotes-1
-			   int randNum = frandNum(0, numQuotes - 1);
-			   // Allocate memory for quote indices and lengths
-			   long int* quoteIndices = fquoteIndices(numQuotes);
-			   int* quoteLengths = fquoteLength(numQuotes, quoteIndices);
-
-			   // Allocate memory for the buffer to store the message
-			   char buffer[MAX_QUOTE_LENGTH];
-
-			   // Get the random message from the file
-			   GetMessageFromFile(buffer, MAX_QUOTE_LENGTH, randNum, numQuotes, quoteIndices, quoteLengths);
-
-			   // Transmit the random message
-			   transmitMessage(buffer);
-
-			   // Free allocated memory
-			   free(quoteIndices);
-			   free(quoteLengths);
-			   */
