7e4aa5297ee9ba6c2452874a38e7bd41ff442c26
JesseRussell2240 <jrussell2240@conestogac.on.ca>
2023-11-23 20:49:28 -0500

    Intgrated headers into transmission

--- Audio VS download/Audio VS Download/Transmission.h
+++ Audio VS download/Audio VS Download/Transmission.h
@@ -1,6 +1,19 @@
 #pragma once
 
 
+typedef struct header Header;
+
+struct header {
+	short int sid;
+	short int rid;
+	char priority;
+	short int seqNum;
+	long int payloadSize;		// Number of bytes in payload after this header
+	char payLoadType;			// 0: Text, 1: Audio, 2: Image etc.
+	char encryption;			// 0: None, 1: XOR,	  2: Vigenere	3: Both
+	char compression;			// 0: None, 1: RLE,	  2: Huffman,	3: Both
+};
+
 // Function prototypes
 void setComBits(int bits);
 void setComRate(int rate);
@@ -9,3 +22,7 @@ void transmitMessage(const char* msg);
 void receiveMessages(char* msgBuffer, int* msgLength);
 void transmitAudio( short* audioData, int dataSize);
 void receiveAudio(short* audioData, int dataSize);
+//DWORD receivePayload(Header* Header, void** Payload, HANDLE* hCom, wchar_t* COMPORT, int nComRate, int nComBits, COMMTIMEOUTS timeout);
+//void transmitPayload(Header* Header, void* Payload, HANDLE* hCom, wchar_t* COMPORT, int nComRate, int nComBits, COMMTIMEOUTS timeout);
+void transmitPayload(Header* txHeader, void* txPayload, HANDLE* hCom, wchar_t* COMPORT, int nComRate, int nComBits, COMMTIMEOUTS timeout);
+DWORD receivePayload(Header* rxHeader, void** rxPayload, HANDLE* hCom, wchar_t* COMPORT, int nComRate, int nComBits, COMMTIMEOUTS timeout);
